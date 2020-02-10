03d4cba63bafa327b3442d41eb4ab68bbd4438f8
codinghuang <2812240764@qq.com>
2020-02-10 13:56:26 +0000

    test: add TEST(string, strnaddr)

--- core-tests/src/string.cpp
+++ core-tests/src/string.cpp
@@ -50,3 +50,43 @@ TEST(string, strnpos)
         ASSERT_EQ(-1, pos);
     }
 }
+
+TEST(string, strnaddr)
+{
+    {
+        char haystack[1024];
+        uint32_t haystack_length;
+        char needle[8];
+        uint32_t needle_length;
+        const char *pos;
+
+        strcpy(haystack, "hello world");
+        haystack_length = sizeof("hello world") - 1;
+        haystack[haystack_length] = 0;
+        strcpy(needle, " ");
+        needle_length = sizeof(" ") - 1;
+        needle[needle_length] = 0;
+
+        pos = swoole_strnaddr(haystack, haystack_length, needle, needle_length);
+        ASSERT_EQ(haystack + 5, pos);
+    }
+    {
+        char haystack[1024];
+        uint32_t haystack_length;
+        char needle[8];
+        uint32_t needle_length;
+        const char *pos;
+
+        strcpy(haystack, "hello world");
+        haystack_length = sizeof("hello world") - 1;
+        haystack[haystack_length] = 0;
+        strcpy(needle, "*");
+        needle_length = sizeof("*") - 1;
+        needle[needle_length] = 0;
+
+        pos = swoole_strnaddr(haystack, haystack_length, needle, needle_length);
+        ASSERT_EQ(NULL, pos);
+    }
+}
+
+
