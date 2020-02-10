fd6ccddc95f7f5e7e6f3922a2b498f66f2d3d941
codinghuang <2812240764@qq.com>
2020-02-10 13:50:07 +0000

    test: add TEST(string, strnpos)

--- core-tests/src/string.cpp
+++ core-tests/src/string.cpp
@@ -12,3 +12,41 @@ TEST(string, rtrim)
     swoole_rtrim(buf, strlen(buf));
     ASSERT_EQ(strlen(buf), 0);
 }
+
+TEST(string, strnpos)
+{
+    {
+        char haystack[1024];
+        uint32_t haystack_length;
+        char needle[8];
+        uint32_t needle_length;
+        int pos;
+
+        strcpy(haystack, "hello world");
+        haystack_length = sizeof("hello world") - 1;
+        haystack[haystack_length] = 0;
+        strcpy(needle, " ");
+        needle_length = sizeof(" ") - 1;
+        needle[needle_length] = 0;
+
+        pos = swoole_strnpos(haystack, haystack_length, needle, needle_length);
+        ASSERT_EQ(pos, 5);
+    }
+    {
+        char haystack[1024];
+        uint32_t haystack_length;
+        char needle[8];
+        uint32_t needle_length;
+        int pos;
+
+        strcpy(haystack, "hello world");
+        haystack_length = sizeof("hello world") - 1;
+        haystack[haystack_length] = 0;
+        strcpy(needle, "*");
+        needle_length = sizeof("*") - 1;
+        needle[needle_length] = 0;
+
+        pos = swoole_strnpos(haystack, haystack_length, needle, needle_length);
+        ASSERT_EQ(-1, pos);
+    }
+}
